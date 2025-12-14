4f65878998a41f79304c597ee9970fd16ee25945
benjitusk <benjitusk1@gmail.com>
2025-12-14 23:05:34 +0200

    Add platform variants and rendering improvements

--- Pr02/games/demo/js/Platform.js
+++ Pr02/games/demo/js/Platform.js
@@ -12,16 +12,32 @@ export default class Platform extends Entity {
 	constructor(x, y, variant) {
 		super();
 		this.position = new Vector(x, y);
-		this.size = new Vector(40, 5);
+		this.size = new Vector(60, 8);
+		this.variant = variant;
 	}
 
 	/**
 	 * Renders the platform on the canvas.
 	 * @param {CanvasRenderingContext2D} ctx
+	 * @param {boolean} debug
 	 */
-	render(ctx) {
+	render(ctx, debug) {
 		ctx.fillStyle = 'green';
-		ctx.fill();
+		ctx.beginPath();
 		ctx.roundRect(this.position.x, this.position.y, this.size.x, this.size.y, 5);
+		ctx.fill();
+
+		// Debug rendering, show platform bounds and position
+		if (!debug) return;
+		ctx.strokeStyle = 'red';
+		ctx.strokeRect(this.position.x, this.position.y, this.size.x, this.size.y);
+		ctx.fillStyle = 'black';
+		ctx.font = '10px Arial';
+		ctx.fillText(
+			`(${this.position.x.toFixed(0)}, ${this.position.y.toFixed(0)})`,
+			this.position.x,
+			this.position.y - 5
+		);
+		ctx.fillText(`Size: (${this.size.x}, ${this.size.y})`, this.position.x, this.position.y + 20);
 	}
 }
