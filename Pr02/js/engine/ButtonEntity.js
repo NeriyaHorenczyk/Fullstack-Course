7302c77bd6089a5408bbdba68f46fdad053bf8e2
Horenczyk Neriya <ner13579@gmail.com>
2025-12-23 12:30:15 +0200

    added restart button

--- Pr02/js/engine/ButtonEntity.js
+++ Pr02/js/engine/ButtonEntity.js
@@ -35,12 +35,30 @@ export default class ButtonEntity extends Entity {
                 mouseY >= this.position.y &&
                 mouseY <= this.position.y + this.size.y;
         };
+
+        /** @type {(event: MouseEvent) => void} */
+        this.mouseClickListener = (event) => {
+            const mouseX = event.clientX;
+            const mouseY = event.clientY;
+            if (
+                mouseX >= this.position.x &&
+                mouseX <= this.position.x + this.size.x &&
+                mouseY >= this.position.y &&
+                mouseY <= this.position.y + this.size.y
+            ) {
+                this.onClick();
+            }
+        };
+
+        window.addEventListener('click', this.mouseClickListener);
         window.addEventListener('mousemove', this.mouseMoveListener);
     }
 
     onRemove() {
         // @ts-expect-error shhh
         window.removeEventListener('mousemove', this.mouseMoveListener);
+        // @ts-expect-error shhh
+        window.removeEventListener('click', this.mouseClickListener);
     }
 
     /**
@@ -51,5 +69,14 @@ export default class ButtonEntity extends Entity {
         // Draw button background
         ctx.fillStyle = this.isHovered ? '#AAAAAA' : '#CCCCCC';
         ctx.fillRect(this.position.x, this.position.y, this.size.x, this.size.y);
+        // Draw button border
+        ctx.strokeStyle = '#000000';
+        ctx.strokeRect(this.position.x, this.position.y, this.size.x, this.size.y);
+        // Draw button label
+        ctx.fillStyle = '#000000';
+        ctx.font = '20px Arial';
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.fillText(this.label, this.position.x + this.size.x / 2, this.position.y + this.size.y / 2);
     }
 }
