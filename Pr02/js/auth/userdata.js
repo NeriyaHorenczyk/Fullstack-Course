54a85b15294d3b264299db7195df5a07b11d52b7
benjitusk <benjitusk1@gmail.com>
2025-12-31 12:29:21 +0200

    remove unneeded code

--- Pr02/js/auth/userdata.js
+++ Pr02/js/auth/userdata.js
@@ -1,12 +1,3 @@
-export default function getUserSummary(username) {
-    const storedData = localStorage.getItem(`user:${username}`) || '{}';
-    const userData = JSON.parse(storedData);
-    return {
-        username,
-        hasAccount: !!userData.hashedCreds,
-    };
-}
-
 export function storeUserData(username, data) {
     const storedData = localStorage.getItem(`user:${username}`) || '{}';
     const userData = JSON.parse(storedData);
