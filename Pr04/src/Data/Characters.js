44988eb428af20568ff6a626565c6d330206f7f6
Neriya <ner13579@gmail.com>
2026-03-29 15:05:22 +0300

    changed the characters structure to be like real keyboard

--- Pr04/src/Data/Characters.js
+++ Pr04/src/Data/Characters.js
@@ -1,13 +1,36 @@
-const englishLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-const englishUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-const hebrewLetters = ['א','ב','ג','ד','ה','ו','ז','ח','ט','י','כ','ך','ל','מ','ם','נ','ן','ס','ע','פ','ף','צ','ץ','ק','ר','ש','ת'];
+const englishLower = [
+    ['q','w','e','r','t','y','u','i','o','p','[',']'],
+    ['a','s','d','f','g','h','j','k','l', ';',"'"],
+    ['z','x','c','v','b','n','m',',','.','/']
+]
+const englishUpper = [
+    ['Q','W','E','R','T','Y','U','I','O','P','{','}'],
+    ['A','S','D','F','G','H','J','K','L', ':','"'],
+    ['Z','X','C','V','B','N','M',',','.','/']
+]
+const hebrewLetters = [
+    ['/',"'",'ק','ר','א','ט','ו','ן','ם','פ','[',']'],
+    ['ש','ד','ג','כ','ע','י','ח','ל','ך', ';',"'"],
+    ['ז','ס','ב','ה','נ','מ','צ','ת','ץ','.']
+    ]
+
 //TODO: Add French characters
 
-const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-const special = ['!','?',',','.','@','#','$','%','&','(',')','-','+','=','"',"'",' '];
+const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+const special =  ['!','@','#','$','%','^','&','*','(',')'];
 
 export const characters = {
-    English: { primary: [...englishLower, ...numbers, ...special], secondary: [...englishUpper, ...numbers, ...special] },
-    Hebrew: { primary: [...hebrewLetters, ...numbers, ...special]},
-    //TODO: Add the French Array
+    English: { 
+        primary: englishLower, 
+        secondary: englishUpper,
+        numbers: numbers,
+        special: special
+    },
+    Hebrew: { 
+        primary: hebrewLetters,
+        numbers: numbers,
+        special: special
+    }
+
+    //TODO: Add French characters
 }
