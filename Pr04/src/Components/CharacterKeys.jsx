7c171a3c71000a4c60d33db50e27ce4844b01dbe
Neriya <ner13579@gmail.com>
2026-03-29 15:16:52 +0300

    Added the characterKeys component

---
+++ Pr04/src/Components/CharacterKeys.jsx
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+import styles from './CharacterKeys.module.css'
+import { characters } from '../Data/Characters'
+import Key from './Key'
+
+
+function CharacterKeys({language="Hebrew", text, setText}) {
+
+    const [isCaps, setIsCaps] = useState(false);
+    const [isSpecial, setIsSpecial] = useState(false);
+
+    const letterRows = characters[language][isCaps && characters[language].secondary ? "secondary" : "primary"]
+    const numberRow = isSpecial ? characters[language].special : characters[language].numbers
+    
+    return (
+        <section className={styles.characterKeys}>
+
+            {/* Number / Special row */}
+            <div className={styles.row}>
+                {numberRow.map((char, index) => (
+                    <Key key={index} label={char} onClick={() => setText(text + char)} />
+                ))}
+            </div>
+
+            {/* Letter rows */}
+            {letterRows.map((row, rowIndex) => (
+                <div className={styles.row} key={rowIndex}>
+                    {row.map((char, charIndex) => (
+                        <Key key={charIndex} label={char} onClick={() => setText(text + char)} />
+                    ))}
+                </div>
+            ))}
+
+            {/* Control keys row */}
+            <div className={styles.row}>
+                {language !== "Hebrew" && (
+                    <Key label="Caps" onClick={() => setIsCaps(!isCaps)} isSelected={isCaps} />
+                )}
+                <Key label={isSpecial ? "123" : "!@#"} onClick={() => setIsSpecial(!isSpecial)} isSelected={isSpecial} />
+            </div>
+
+        </section>
+      );
+}
+
+export default CharacterKeys;
