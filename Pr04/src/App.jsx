945812dfb9e32d2d20a4f5af3ee38611ce1dfa85
Neriya <ner13579@gmail.com>
2026-03-29 12:53:27 +0300

    Made the basic react vite files

---
+++ Pr04/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='app'>
+      <p>{count}</p>
+      <button onClick={()=> setCount(count - 1)}>Decrement</button>
+      <button onClick={()=> setCount(0)}>Reset</button>
+      <button onClick={()=> setCount(count + 1)}>Increment</button>
+    </div>
+  )
+}
+
+export default App
