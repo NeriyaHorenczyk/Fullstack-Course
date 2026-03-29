3f2bf8389a57e4ff5d03bbc098b4a6a27332f8d1
Neriya <ner13579@gmail.com>
2026-03-29 15:38:58 +0300

    Added the ActionKey component and imported the icons

---
+++ Pr04/src/Components/ActionKeys.jsx
@@ -0,0 +1,35 @@
+import styles from './CSS/ActionKeys.module.css'
+import Key from './Key'
+import { FaUndo } from "react-icons/fa";
+import { FaDeleteLeft } from "react-icons/fa6";
+import { MdDelete } from "react-icons/md";
+
+function ActionKeys({text, setText, history, setHistory}) {
+    return ( 
+        <section className={styles.actionKeys}>
+            
+            <Key label={<FaDeleteLeft />} onClick={() =>{ 
+                setHistory([...history, text]);
+                setText(text.slice(0, -1))
+                }} />
+            
+            <Key label="Delete Word" onClick={() => { 
+                setHistory([...history, text]);
+                setText(text.trim().split(" ").slice(0, -1).join(" ") + " "); 
+                }} />
+            
+            <Key label={<MdDelete />} onClick={() => { 
+                setHistory([...history, text]); 
+                setText(""); 
+                }} />
+
+            <Key label={<FaUndo />} onClick={()=>{
+                if (history.length === 0) return;
+                setText(history[history.length - 1]);
+                setHistory(history.slice(0, -1));
+                }} />
+        </section>
+     );
+}
+
+export default ActionKeys;
